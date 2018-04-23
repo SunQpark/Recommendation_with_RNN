38144318ab8733f9f182af0f23c0035f02f6c5ce
SunQpark <SunQ0313@gmail.com>
2018-04-23 22:19:26 +0900

    pytorch template added

---
+++ base/base_trainer.py
@@ -0,0 +1,114 @@
+import os
+import math
+import logging
+import torch
+from utils.util import ensure_dir
+
+
+class BaseTrainer:
+    """
+    Base class for all trainers
+    """
+    def __init__(self, model, loss, metrics, optimizer, epochs,
+                 save_dir, save_freq, resume, verbosity, training_name,
+                 with_cuda, train_logger=None, monitor='loss', monitor_mode='min'):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.model = model
+        self.loss = loss
+        self.metrics = metrics
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.save_freq = save_freq
+        self.verbosity = verbosity
+        self.training_name = training_name
+        self.train_logger = train_logger
+        self.with_cuda = with_cuda and torch.cuda.is_available()
+        if with_cuda and not torch.cuda.is_available():
+            self.logger.warning('Warning: There\'s no CUDA support on this machine, '
+                                'training is performed on CPU.')
+        self.monitor = monitor
+        self.monitor_mode = monitor_mode
+        assert monitor_mode == 'min' or monitor_mode == 'max'
+        self.monitor_best = math.inf if monitor_mode == 'min' else -math.inf
+        self.start_epoch = 1
+        self.checkpoint_dir = os.path.join(save_dir, training_name)
+        ensure_dir(self.checkpoint_dir)
+        if resume:
+            self._resume_checkpoint(resume)
+
+    def train(self):
+        """
+        Full training logic
+        """
+        for epoch in range(self.start_epoch, self.epochs+1):
+            result = self._train_epoch(epoch)
+            log = {'epoch': epoch}
+            for key, value in result.items():
+                if key == 'metrics':
+                    for i, metric in enumerate(self.metrics):
+                        log[metric.__name__] = result['metrics'][i]
+                elif key == 'val_metrics':
+                    for i, metric in enumerate(self.metrics):
+                        log['val_'+metric.__name__] = result['val_metrics'][i]
+                else:
+                    log[key] = value
+            if self.train_logger is not None:
+                self.train_logger.add_entry(log)
+                if self.verbosity >= 1:
+                    for key, value in log.items():
+                        self.logger.info('    {:15s}: {}'.format(str(key), value))
+            if (self.monitor_mode == 'min' and log[self.monitor] < self.monitor_best)\
+                    or (self.monitor_mode == 'max' and log[self.monitor] > self.monitor_best):
+                self.monitor_best = log[self.monitor]
+                self._save_checkpoint(epoch, log['loss'], save_best=True)
+            if epoch % self.save_freq == 0:
+                self._save_checkpoint(epoch, log['loss'])
+
+    def _train_epoch(self, epoch):
+        """
+        Training logic for an epoch
+
+        :param epoch: Current epoch number
+        """
+        raise NotImplementedError
+
+    def _save_checkpoint(self, epoch, loss, save_best=False):
+        """
+        Saving checkpoints
+
+        :param epoch: Current epoch number
+        :param loss: Prefix of checkpoint name
+        :param save_best: If True, rename the saved checkpoint to 'model_best.pth.tar'
+        """
+        arch = type(self.model).__name__
+        state = {
+            'epoch': epoch,
+            'logger': self.train_logger,
+            'arch': arch,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'monitor_best': self.monitor_best,
+        }
+        filename = os.path.join(self.checkpoint_dir,
+                                'checkpoint_epoch{:02d}_loss_{:.5f}.pth.tar'.format(epoch, loss))
+        torch.save(state, filename)
+        if save_best:
+            os.rename(filename, os.path.join(self.checkpoint_dir, 'model_best.pth.tar'))
+            self.logger.info("Saving current best: {} ...".format('model_best.pth.tar'))
+        else:
+            self.logger.info("Saving checkpoint: {} ...".format(filename))
+
+    def _resume_checkpoint(self, resume_path):
+        """
+        Resume from saved checkpoints
+
+        :param resume_path: Checkpoint path to be resumed
+        """
+        self.logger.info("Loading checkpoint: {} ...".format(resume_path))
+        checkpoint = torch.load(resume_path)
+        self.start_epoch = checkpoint['epoch'] + 1
+        self.monitor_best = checkpoint['monitor_best']
+        self.model.load_state_dict(checkpoint['state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.train_logger = checkpoint['logger']
+        self.logger.info("Checkpoint '{}' (epoch {}) loaded".format(resume_path, self.start_epoch))
---
+++ train.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+import torch.optim as optim
+from model.model import MnistModel
+from model.loss import my_loss
+from model.metric import my_metric, my_metric2
+from data_loader import MnistDataLoader
+from trainer import Trainer
+from logger import Logger
+
+logging.basicConfig(level=logging.INFO, format='')
+
+parser = argparse.ArgumentParser(description='PyTorch Template')
+parser.add_argument('-b', '--batch-size', default=32, type=int,
+                    help='mini-batch size (default: 32)')
+parser.add_argument('-e', '--epochs', default=32, type=int,
+                    help='number of total epochs (default: 32)')
+parser.add_argument('--resume', default='', type=str,
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--verbosity', default=2, type=int,
+                    help='verbosity, 0: quiet, 1: per epoch, 2: complete (default: 2)')
+parser.add_argument('--save-dir', default='saved', type=str,
+                    help='directory of saved model (default: saved)')
+parser.add_argument('--save-freq', default=1, type=int,
+                    help='training checkpoint frequency (default: 1)')
+parser.add_argument('--data-dir', default='datasets', type=str,
+                    help='directory of training/testing data (default: datasets)')
+parser.add_argument('--validation-split', default=0.1, type=float,
+                    help='ratio of split validation data, [0.0, 1.0) (default: 0.1)')
+parser.add_argument('--no-cuda', action="store_true",
+                    help='use CPU instead of GPU')
+
+
+def main(args):
+    # Model
+    model = MnistModel()
+    model.summary()
+
+    # A logger to store training process information
+    train_logger = Logger()
+
+    # Specifying loss function, metric(s), and optimizer
+    loss = my_loss
+    metrics = [my_metric, my_metric2]
+    optimizer = optim.Adam(model.parameters())
+
+    # Data loader and validation split
+    data_loader = MnistDataLoader(args.data_dir, args.batch_size, shuffle=True)
+    valid_data_loader = data_loader.split_validation(args.validation_split)
+
+    # An identifier for this training session
+    training_name = type(model).__name__
+
+    # Trainer instance
+    trainer = Trainer(model, loss, metrics,
+                      data_loader=data_loader,
+                      valid_data_loader=valid_data_loader,
+                      optimizer=optimizer,
+                      epochs=args.epochs,
+                      train_logger=train_logger,
+                      save_dir=args.save_dir,
+                      save_freq=args.save_freq,
+                      resume=args.resume,
+                      verbosity=args.verbosity,
+                      training_name=training_name,
+                      with_cuda=not args.no_cuda,
+                      monitor='val_my_metric',
+                      monitor_mode='max')
+
+    # Start training!
+    trainer.train()
+
+    # See training history
+    print(train_logger)
+
+
+if __name__ == '__main__':
+    main(parser.parse_args())
